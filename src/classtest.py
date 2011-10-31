5329c16789404ac52dcc89fb71f54c8d2b8ebcfe
Rich <yoyofu@localhost>
2011-10-31 15:42:30 +0800

    100.10.31 1

---
+++ src/classtest.py
@@ -0,0 +1,18 @@
+'''
+Created on 2011/10/31
+
+@author: Administrator
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+     
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
